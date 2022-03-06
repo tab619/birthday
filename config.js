70ea503f032868e36149c13459231bd618cecf69
Tab619 <52664521+tab619@users.noreply.github.com>
2022-03-06 18:15:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
+        turn_on: "点我开始",
+        play: "上音乐",
+        bannar_coming: "搞点颜色",
+        balloons_flying: "好像少点东西？",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        wish_message: "生日快乐！奕璇",
         story: "A MESSAGE FOR YOU",
     }
 };
